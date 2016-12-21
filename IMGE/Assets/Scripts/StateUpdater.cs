430a8cf610f3f61a3bec7008fbc314bc17ffb912
jungo <ga45jas@mytum.de>
2016-12-21 20:26:40 +0100

    Even more Code spawned!

--- IMGE/Assets/Scripts/StateUpdater.cs
+++ IMGE/Assets/Scripts/StateUpdater.cs
@@ -3,40 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StateUpdater : MonoBehaviour {
+public static class StateUpdater : MonoBehaviour {
     /*Gebrauch:
      * Speichert Werte wie HP und Munition
      * Updatet davon abhängig die Anzeigen am Bildschirm
      * 
      */
-    int HP;//Muss 0-100 sein
-    int munition;
-    int score;
-    public Image Healthbar;
-    public Image Munition;
-    public Text Score;
+    private static int HP;//Muss 0-100 sein
+    private static int munition;
+    private static int score;
+    public static Image Healthbar;
+    public static Image Munition;
+    public static Text Score;
 
 	// Use this for initialization
-	void Start () {
+	static void Start () {
 		//Einstellen von Anzeigen (wäre hier schöner)
         
+        //Test
+        test();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	static void Update () {
+        test2();
 	}
 
-    public void UpdateScore(int deltaHP, int deltaMunition, int deltaScore)
+    public static void UpdateStats(int deltaHP, int deltaMunition, int deltaScore)
     {
         HP += deltaHP;
         munition += deltaMunition;
         score += deltaScore;
+
         //Lebensleiste aktualisieren
+        //TODO
 
         //Munitionsleiste aktualisieren
-        Munition.rectTransform.localScale.Set(HP, 20, 0);//Noch Position setzen
-        Munition.rectTransform.localPosition.Set(345.5f+30-munition, -212f, 0f);
+        Munition.rectTransform.localScale.Set(munition, 20, 0);
+        Munition.rectTransform.localPosition.Set(390f-(munition-6.4f)/2, -212f, 0f);
+
         //Score aktualisieren
+        //TODO
+    }
+    private static void test()
+    {
+        HP = 100;
+        munition = 30;
+        score = 0;
+    }
+    private static void test2()
+    {
+        if (Input.GetButtonDown("SPACE"))
+        {
+            UpdateStats(0, -1, 0);
+        }
     }
 }
