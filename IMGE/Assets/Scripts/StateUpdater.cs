5c4a94aff74ed50aff5be940aa67ead377167e4f
jungo <ga45jas@mytum.de>
2016-12-22 15:53:59 +0100

    How many bullets are left?

--- IMGE/Assets/Scripts/StateUpdater.cs
+++ IMGE/Assets/Scripts/StateUpdater.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public static class StateUpdater : MonoBehaviour {
+public class StateUpdater : MonoBehaviour{
     /*Gebrauch:
      * Speichert Werte wie HP und Munition
      * Updatet davon abhängig die Anzeigen am Bildschirm
@@ -12,20 +12,21 @@ public static class StateUpdater : MonoBehaviour {
     private static int HP;//Muss 0-100 sein
     private static int munition;
     private static int score;
-    public static Image Healthbar;
-    public static Image Munition;
-    public static Text Score;
+    private static Image Healthbar;
+    private static Image Munition;
+    private static Image Score;
 
 	// Use this for initialization
-	static void Start () {
+	void Start () {
 		//Einstellen von Anzeigen (wäre hier schöner)
-        
+        //GameObject tmp=GameObject.Find("Canvas");
+        Munition = GameObject.Find("Bullet-State-IMG").GetComponent<Image>();
         //Test
         test();
 	}
 	
 	// Update is called once per frame
-	static void Update () {
+	void Update () {
         test2();
 	}
 
@@ -39,23 +40,26 @@ public static class StateUpdater : MonoBehaviour {
         //TODO
 
         //Munitionsleiste aktualisieren
-        Munition.rectTransform.localScale.Set(munition, 20, 0);
-        Munition.rectTransform.localPosition.Set(390f-(munition-6.4f)/2, -212f, 0f);
 
+        Munition.rectTransform.sizeDelta = new Vector2(munition*6.4f, 12.8f);
+        //Munition.rectTransform.localPosition.Set(390f - (munition*6.4f - 6.4f) / 2, -212f, 0f);
+        Munition.rectTransform.Translate(new Vector3((deltaMunition * 6.4f) / 2,0f, 0f));
         //Score aktualisieren
         //TODO
     }
     private static void test()
     {
         HP = 100;
-        munition = 30;
+        //munition = 30;
+        UpdateStats(0, 30, 0);
         score = 0;
     }
     private static void test2()
     {
-        if (Input.GetButtonDown("SPACE"))
+        if (Input.GetButtonDown("Jump"))
         {
             UpdateStats(0, -1, 0);
+            Debug.Log("Bin da!");
         }
     }
 }
