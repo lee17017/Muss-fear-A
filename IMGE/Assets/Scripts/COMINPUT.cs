4d1ef7aa166f075d15e901581523018fbccb699e
Liou <liou@gmx.at>
2017-01-31 15:40:01 +0100

    COMINPUT

---
+++ IMGE/Assets/Scripts/COMINPUT.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COMINPUT : MonoBehaviour {
+
+    public string p1, p2;
+	// Use this for initialization
+	void Start () {
+        if (!p1.Equals("") && !p2.Equals(""))
+        {
+
+            InputManager.setStream(p1, p2);
+            InputManager.Init();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
